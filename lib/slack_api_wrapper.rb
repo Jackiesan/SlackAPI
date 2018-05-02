609524fadec00ee23dfb710e0c257629f7478c72
Jackie <jonofre16@hotmail.com>
2018-05-02 09:23:59 -0700

    Added lib class

--- lib/slack_api_wrapper.rb
+++ lib/slack_api_wrapper.rb
@@ -1,5 +1,38 @@
 require 'httparty'
 
 class SlackApiWrapper
-  # Your code here!
+  URL = "https://slack.com/api/"
+  TOKEN = ENV["SLACK_TOKEN"]
+
+  def self.list_channels
+    response = HTTParty.get("#{URL}channels.list?token=#{TOKEN}")
+    # ? query
+    channel_list = []
+
+    if response["channels"]
+      response["channels"].each do |channel|
+        channel_list << Channel.new(channel["name"], channel["id"])
+      end
+    end
+
+    return channel_list
+  end
+
+  def self.send_message(channel, message)
+    message_url = "#{URL}chat.postMessage"
+    response = HTTParty.post(message_url, body: {
+      "token" => TOKEN,
+      "channel" => channel,
+      "text" => message,
+      "username" => "Bob",
+      "icon_emoji" => ":epic_win:",
+      "as_user" => "false"
+    },
+    :headers => { 'Content-Type' => 'application/x-www-form-urlencoded' }
+  )
+  return response.success?
+end
+
+
+
 end
